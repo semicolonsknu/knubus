f0f3d4e9b839a155c76d9412c0eeb68845ee88f2
yuni <hys990730@gmail.com>
2024-03-09 23:29:59 +0900

    feat: Title 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center; 
+  justify-content: center; 
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text> 
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Text>knubus</Text>
+        <StatusBar style="auto" />
+      </Container>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
