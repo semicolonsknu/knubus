f4eeac779076df194ec05ccf4d5daf24be944b1a
KimSongMok <mok0618@naver.com>
2024-03-20 03:19:29 +0900

    first commit

---
+++ KNUBUS/src/main/java/semicolon/knubus/KnubusApplication.java
@@ -0,0 +1,13 @@
+package semicolon.knubus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnubusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KnubusApplication.class, args);
+  }
+
+}
