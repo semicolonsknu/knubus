ec2c92368e02422da138027a6d4d6688050d538e
yuni <hys990730@gmail.com>
2024-03-20 02:34:04 +0900

    design: 컴포넌트 범위

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import Navbar from './src/components/Navbar'
 import Header from './src/components/Header'
 import Footer from './src/components/Footer'
@@ -9,15 +9,63 @@ import TimeTablePage from './src/pages/TimeTablePage'
 
 const App = () => {
   return (
-    <View>
-      <Navbar />
-      <Header />
-      <HomePage />
-      <RoadMapPage />
-      <TimeTablePage />
-      <Footer />
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text>------------- Header 영역 -------------</Text>
+        <Header />
+      </View>
+
+      <View style={styles.section}>
+        <Text>------------- Navbar 영역 -------------</Text>
+        <Navbar />
+      </View>
+
+      <View style={styles.section}>
+        <Text>------------- Page 영역 -------------</Text>
+        <View style={styles.subSection}>
+          <Text>--- HomePage 영역 ---</Text>
+          <HomePage />
+        </View>
+
+        <View style={styles.subSection}>
+          <Text>--- RoadMapPage 영역 ---</Text>
+          <RoadMapPage />
+        </View>
+
+        <View style={styles.subSection}>
+          <Text>--- TimeTablePage 영역 ---</Text>
+          <TimeTablePage />
+        </View>
+      </View>
+
+      <View style={styles.section}>
+        <Text>------------- Footer 영역 -------------</Text>
+        <Footer />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  section: {
+    borderColor: 'red',
+    borderWidth: 5,
+    margin: 5,
+    padding: 5,
+    width: '90%',
+  },
+  subSection: {
+    borderColor: 'blue',
+    borderWidth: 3,
+    margin: 5,
+    padding: 5,
+    width: '90%',
+  },
+})
 
 export default App
