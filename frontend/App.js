c9cf11f5c0a409286896cb589c006eaaf92685b6
yuni <hys990730@gmail.com>
2024-03-28 21:29:59 +0900

    feat: 전체 상단바 가려지지 않도록 수정

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native'
+import Header from './src/components/Header' // Adjust the path as necessary
 import Navigation from './src/Navigation'
 
-export default function App() {
-  return <Navigation />
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Header />
+      <Navigation />
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+export default App
