9933f21e7bddf11e7d9e0d5e55b9d06d249f250a
yuni <hys990730@gmail.com>
2024-03-28 21:33:17 +0900

    feat: react-native-image-zoom-viewer 사용으로 이미지 zoom 기능 구현

--- frontend/src/pages/MapScreen.js
+++ frontend/src/pages/MapScreen.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Dimensions } from 'react-native'
+import ImageViewer from 'react-native-image-zoom-viewer'
 
-const RoadMapPage = () => {
+// 화면의 너비와 높이를 구합니다.
+const { width, height } = Dimensions.get('window')
+
+const images = [
+  {
+    // 이미지의 경로를 지정합니다. 로컬 이미지 대신 웹 이미지 URL도 사용할 수 있습니다.
+    url: '',
+    props: {
+      // 여기에 로컬 이미지를 require로 불러오세요
+      source: require('../../assets/public/Map.png'),
+    },
+  },
+]
+
+const MapScreen = () => {
   return (
-    <View>
-      <Text>RoadMap Page</Text>
+    <View style={{ flex: 1, width: width, height: height }}>
+      <ImageViewer imageUrls={images} />
     </View>
   )
 }
 
-export default RoadMapPage
+export default MapScreen
