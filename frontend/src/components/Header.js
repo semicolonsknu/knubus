46f625681c2501a175f1e24b6bcca21c298ea970
Kuheewon <selly3109@naver.com>
2024-03-31 09:19:09 +0900

    feat 로고를 누르면 교내 순환버스 안내로 이동하도록 클릭

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, StyleSheet,TouchableOpacity,Linking } from 'react-native'
 
-// 로고를 클릭하면 https://wwwk.kangwon.ac.kr/www/contents.do?key=2414& 링크가 인터넷 앱에서 열리도록 수정 @구희원
-// 로고 교체 @구희원
+// 로고를 클릭하면 https://wwwk.kangwon.ac.kr/www/contents.do?key=2414& 링크가 인터넷 앱에서 열리도록 수정 @구희원 (완료)
+// 로고 교체 (완료)
 const Header = () => {
+  const handleLogoPress = () => {
+    Linking.openURL('https://wwwk.kangwon.ac.kr/www/contents.do?key=2414&');
+  };
+  
   return (
     <View style={styles.headerContainer}>
+      <TouchableOpacity onPress = {handleLogoPress}>
       <Image
-        source={require('../../assets/public/Logo.png')}
+        source={require('../../assets/public/logo.png')}
         resizeMode="contain"
         style={styles.logo}
       />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -19,8 +25,8 @@ const styles = StyleSheet.create({
   headerContainer: {
     width: '100%',
     height: 50,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
   },
   logo: {
