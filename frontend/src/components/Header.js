cdd705d405a4306fd3f3ab0b7d49da400b1f3a5f
yuni <hys990730@gmail.com>
2024-04-15 07:20:19 +0900

    file: 로고 교체

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { View, Image, StyleSheet,TouchableOpacity,Linking } from 'react-native'
-
+import {
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from 'react-native'
 
 // 로고를 클릭하면 https://wwwk.kangwon.ac.kr/www/contents.do?key=2414& 링크가 인터넷 앱에서 열리도록 수정 @구희원 (완료)
 // 로고 교체 (완료)
 const Header = () => {
   const handleLogoPress = () => {
-    Linking.openURL('https://wwwk.kangwon.ac.kr/www/contents.do?key=2414&');
-  };
-  
+    Linking.openURL('https://wwwk.kangwon.ac.kr/www/contents.do?key=2414&')
+  }
+
   return (
     <View style={styles.headerContainer}>
-      <TouchableOpacity onPress = {handleLogoPress}>
-      <Image
-        source={require('../../assets/public/Logo.png')}
-        resizeMode="contain"
-        style={styles.logo}
-      />
+      <TouchableOpacity onPress={handleLogoPress}>
+        <Image
+          source={require('../../assets/KNUB.png')}
+          resizeMode="contain"
+          style={styles.logo}
+        />
       </TouchableOpacity>
     </View>
   )
