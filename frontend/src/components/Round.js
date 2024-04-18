9a741bbc834a1a24bbe12a52dcd8be11229a784b
Kuheewon <selly3109@naver.com>
2024-04-18 22:03:31 +0900

    feat round.js 예외 처리

--- frontend/src/components/Round.js
+++ frontend/src/components/Round.js
@@ -208,7 +208,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     marginBottom: 10,
-    justifyContent: 'center',
+    justifyContent: 'left',
   },
   button: {
     backgroundColor: '#4A90E2',
