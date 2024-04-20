2d8bdf486d20bb13764d9dd761769ee59dbb9d93
yuni <hys990730@gmail.com>
2024-04-21 01:27:51 +0900

    build: 깃허브 설정

--- frontend/src/data/apiKey.js
+++
@@ -1,4 +0,0 @@
-export const dateApiKey =
-  'ZvYPMHcdP5th36amVoHJGtlGw7hwp%2B8HPSBiZJRe2OzO0t6Bh3iqj5UE15%2Fn5LBpkYYILdb3XQ4ElOFgMWha6A%3D%3D'
-
-export const googleApiKey = 'AIzaSyCK_c0GE6lyMJC4Uax2uujoMDEIIVmAgPI'
